1a48a1e65801ee8db1051eff731b47180d709c74
Liad Cohen <liad.code@gmail.com>
2019-05-23 15:32:08 +0300

    changed example to mix cpp string with required c-style char* string

--- inputThenMD5.cpp
+++ inputThenMD5.cpp
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 #include <string>
 #include "md5.h"
 
+using std::cout;
 
 int main(){
     char name[32]; //max chars after md5.
     printf("Enter your message: ");
     fgets(name, 20, stdin);  //we read max 20 chars.
     printf("Your message is: %s", name);
-    std::string hashed = "a";
-    // hashed = md5(hashed);
-    printf("Your message after MD5: %s", name);
+    std::string hashed = md5(name);
+    const char * cstr = hashed.c_str();
+    cout << "Your message after MD5: "<<  cstr ;
     return 0;
 }
