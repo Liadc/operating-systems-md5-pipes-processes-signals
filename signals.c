74b172dacb063c4afb2ccf134b8febd7e1ad530c
Liad Cohen <liad.code@gmail.com>
2019-06-04 12:18:55 +0300

    added 2 seconds wait to children entering pause and becoming zombies

--- signals.c
+++ signals.c
@@ -16,40 +16,37 @@
 
 #include <sys/fcntl.h>
 
-int cpid[5];// holds the pids of the children
-int j;// index to cpid 
+int cpid[5];         // holds the pids of the children
+int j;                    // index to cpid 
 
 // function to activate when a signal is caught
-int sigCatcher() {
-  // re-assign the signal catcher
-  signal(SIGINT, sigCatcher);
+int sigCatcher() {  
+  signal(SIGINT, sigCatcher);  // re-assign the signal catcher
   printf("PID %d caught one\n", getpid());
-  if (j > -1)
-    // send signal to next child in cpid
-    kill(cpid[j], SIGINT);
+  if(j > -1)
+    	kill(cpid[j], SIGINT);  // send signal to next child in cpid
 }
 int main() {
-  int i, zombie, status, pid;
-
-  // sets a handler for INT signal
-  signal(SIGINT, sigCatcher);
-
-  for (i = 0; i < 5; i++) {
-    if ((pid = fork()) == 0) { // create new child
-      printf("PID %d ready\n", getpid());
-      j = i - 1;
-      pause();  // wait for signal
-      exit(0);  // end process (become a zombie)
-    } else
-      // Only father updates the cpid array.
-      cpid[i] = pid;
-  }
-  // allow children time to enter pause
-  sleep(2);
-  // send signal to first child
-  kill(cpid[4], SIGINT);
-  for (i = 0; i < 5; i++) {
-    zombie = wait(&status);// collect zombies
+  int i;
+  int zombie;
+  int status;
+  int pid;
+  signal(SIGINT, sigCatcher);    // sets a handler for INT signal
+  for(i=0; i<5; i++){
+    if((pid=fork()) ==  0){      		// create new child
+      	printf("PID %d ready\n", getpid());
+      	j = i-1;
+	pause(); 			// wait for signal
+     	exit(0);  			// end process (become a zombie)
+    }
+    else            			// Only father updates the cpid array.
+      	cpid[i] = pid;	  
+} 
+sleep(2);     			// allow children time to enter pause
+kill(cpid[4], SIGINT);     		// send signal to first child
+sleep(2);                 			// wait for children to become zombies 
+for(i=0; i<5; i++){
+    zombie = wait(&status); 		// collect zombies
     printf("%d is dead\n", zombie);
   }
   exit(0);
